dafd9c7757715699e5490c8955ce1062a20ad4b8
GiulioDangelo <dangelogiulio48@gmail.com>
2024-04-05 14:34:20 +0200

    push

--- src/Gift.java
+++ src/Gift.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Gift {
+public class Gift {
+    String name;
+
+    public Gift(String regalo) {
+        this.name = regalo;
+    }
+
+    public java.lang.String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Gift{" + "name='" + name + '\'' + '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Gift> giftList = new ArrayList<>();
+        boolean flag = true;
+
+        Scanner scan = new Scanner(System.in);
+
+        while (flag) {
+            System.out.println("che regalo vuoi aggiungere alla lista?");
+            String inputRegalo = scan.nextLine();
+
+            Gift nuovoRegalo = new Gift(inputRegalo);
+            giftList.add(nuovoRegalo);
+            System.out.println(giftList.size());
+
+            System.out.println("vuoi aggiungere un altro regalo?");
+            String risposta = scan.nextLine();
+
+            if (risposta.equals("no")) {
+                flag = false;
+                System.out.println(giftList);
+            }
+        }
+
+        scan.close();
+    }
 }
